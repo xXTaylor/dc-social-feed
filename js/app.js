a582b3be3929f0268b01bc76a631d9011bc9761b
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Pro.local>
2019-12-05 19:55:45 -0500

    Example of CORS activation

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+var api_url = 'http://localhost:3000/';
+
+
+function getPosts(successCallback, errorCallback) {
+
+    $.ajax({
+        url: api_url + 'api/posts',
+        success: successCallback,
+        error : errorCallback
+    });
+}
+
+function insertPost(data, successCallback, errorCallback) {
+
+    $.ajax({
+        type: "POST",
+        url: + 'api/posts',
+        data: data,
+        crossDomain: true,
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded', 
+        xhrFields: { withCredentials: true }, 
+        success: successCallback,
+        error : errorCallback
+      });
+}
+
+function createPostItem(data) {
+    let item = `
+        <div class="card card-body bg-light">
+            <div>${data.post}</div>
+            <div>${data.user}</div>
+        </div>
+    `;
+
+    return item;
+}
+
+function insertIntoFeed(item) {
+    $('#feed').append(item);
+}
---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+
+app.options('*', cors())
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
+idCounter = 1;
+
+var posts = [
+    {
+        id: 1,
+        post: "My first post",
+        user : "BOb"
+    },
+    {
+        id: 2,
+        post: "My second post",
+        user : "Jane"
+    }
+];
+
+// GET /api/posts
+
+app.get('/api/posts', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(posts));
+});
+
+// GET /api/posts/:id
+app.get('/api/posts/:id', function(req, res){
+
+    if(posts[req.params.id]) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(posts[req.params.id]));
+    } else {
+        res.status(404).send('Post Not Found')
+    }
+});
+
+// POST /api/posts
+// curl --data "todo=Gotostore" http://localhost:3000/api/posts
+
+app.post('/api/posts', function(req, res){
+    
+    if(req.body.post && req.body.user) {
+        idCounter++;
+        let data = {
+            id: idCounter,
+            post : req.body.post,
+            user: req.body.user
+        };
+
+        posts.push(data);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));
+    } else {
+        res.status(434).send('Post and user is required')
+    }
+
+});
+
+app.listen(3000, function(){
+    console.log('Todo List API is now listening on port 3000...');
+})
